c498b102e721049ad6c0523dfdb8b40e5e4d434d
Daniel Kim <dwkim0101@naver.com>
2023-10-06 13:31:10 +0900

    first commit

---
+++ SwiftUiStudy/SwiftUiStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUiStudy
+//
+//  Created by 김도완 on 10/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUiStudy/SwiftUiStudy/SwiftUiStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiStudyApp.swift
+//  SwiftUiStudy
+//
+//  Created by 김도완 on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
