2b3990f1acd9298c7da99c3126035def019e4fc6
Daniel Kim <dwkim0101@naver.com>
2023-10-08 14:24:26 +0900

    step 2(building list and navigation) completed

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 김도완 on 10/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
